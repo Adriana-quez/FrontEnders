9fa5b35b77a2fba3266876b2ff1dadb63d4c6ce1
Adriana Vasquez <99279815+Adriana-quez@users.noreply.github.com>
2025-11-04 12:57:28 -0500

    Add basic server and basic html for home page
    Also installed some of the necessary packages and moved some public files into app

---
+++ app/server.js
@@ -0,0 +1,14 @@
+let express = require("express");
+let app = express();
+//let apiFile = require("../api/env.json");
+let port = 3000;
+let hostname = "localhost";
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.status(200).send();
+});
+
+app.listen(port, hostname, () => {
+  console.log(`http://${hostname}:${port}`);
+});
