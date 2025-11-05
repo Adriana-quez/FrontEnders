f5c056f789c042505ebadd9a761f9460e37ae14a
Chase Padilla <ninjap419@gmail.com>
2025-11-05 09:34:05 -0500

    added api functionality to get movie info in server using axios

--- app/server.js
+++ app/server.js
@@ -1,12 +1,28 @@
+let axios = require("axios");
 let express = require("express");
 let app = express();
-//let apiFile = require("../api/env.json");
+let apiFile = require("../api/env.json");
+let apiKey = apiFile["api_key"]; // use this to make requests
+let baseUrl = apiFile["api_url"]; // use this to make requests
 let port = 3000;
 let hostname = "localhost";
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.status(200).send();
+});
+
+app.get("/movieInfo", (req, res) => {
+  console.log("Received query string:", req.query);
+  let movieID = req.query.movie_id;
+  console.log("Movie Title:", movieID);
+
+  axios(`${baseUrl}/movie/${movieID}?api_key=${apiKey}`).then(response => {
+    console.log("API response received:", response.data);
+    //res.json(response.data);
+  }).catch(error => {
+    console.log("Error when requesting from API", error);
+  });;
 });
 
 app.listen(port, hostname, () => {
